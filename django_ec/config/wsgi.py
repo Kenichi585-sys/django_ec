4c07d410145ac806d2c6aa6a9807594126d5a046
sample tarou <sample@example.com>
2025-11-22 17:17:48 +0900

    __init__.pyとwsgi.pyを修正

--- django_ec/config/wsgi.py
+++ django_ec/config/wsgi.py
@@ -8,13 +8,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-from pathlib import Path
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.append(str(BASE_DIR))
-
 
 from django.core.wsgi import get_wsgi_application
 
