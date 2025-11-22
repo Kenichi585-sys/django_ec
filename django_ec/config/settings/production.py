05efe85f12a56ab0991b10a01905d5eade136f72
sample tarou <sample@example.com>
2025-11-22 19:03:32 +0900

    本番環境で

--- django_ec/config/settings/production.py
+++ django_ec/config/settings/production.py
@@ -9,7 +9,8 @@ ALLOWED_HOSTS = [os.environ.get('HOST_NAME', 'hc-ec-site.herokuapp.com')]
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True, conn_health_checks=True)
 }
-
+DEBUG_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
+print(f"DEBUG: CLOUDINARY_CLOUD_NAME is: {DEBUG_CLOUD_NAME}")
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
