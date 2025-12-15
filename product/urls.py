d78303b80d1eb0f76fc85cf34ef7cbbf0ac25ec0
sample tarou <sample@example.com>
2025-12-15 21:04:56 +0900

    編集と削除機能を追加

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 
 app_name = 'product'
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('manage/products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('manage/products/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_update'),
+    path('manage/products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Product
 
 
@@ -14,8 +14,26 @@ class ProductDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         related_products = Product.objects.all().order_by('-pk')[:4]
-
         context['related_products'] = related_products
         return context
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'description', 'price', 'image']
+    template_name = 'product/product_create.html'
+    success_url = reverse_lazy('product:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'description', 'price', 'image']
+    template_name = 'product/product_update.html'
+    success_url = reverse_lazy('product:product_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'product/product_delete.html'
+    success_url = reverse_lazy('product:product_list')
