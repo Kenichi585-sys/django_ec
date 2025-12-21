7655d71e1a3d779a35dcf6dcba3035215702c095
sample tarou <sample@example.com>
2025-12-21 19:38:32 +0900

    管理ページ作成。Herokuの再設定完了

--- product/urls.py
+++ product/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, ProductManageListView
 
 
 app_name = 'product'
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('manage/products/', ProductManageListView.as_view(), name='manage_list'),
     path('manage/products/create/', ProductCreateView.as_view(), name='product_create'),
     path('manage/products/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_update'),
     path('manage/products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
--- product/views.py
+++ product/views.py
@@ -1,6 +1,28 @@
+import base64
+from django.http import HttpResponse
+from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Product
+
+def basic_auth_required(func):
+    def wrapper(request, *args, **kwargs):
+        auth_header = request.META.get('HTTP_AUTHORIZATION')
+
+        if auth_header:
+            auth_type, auth_string = auth_header.split(' ', 1)
+            auth_decoded = base64.b64decode(auth_string).decode('utf-8')
+            username, password = auth_decoded.split(':', 1)
+
+            if username == 'admin' and password == 'pw':
+                return func(request, *args, **kwargs)
+            
+        response = HttpResponse("Unauthorized", status=401)
+        response['WWW-Authenticate'] = 'Basic realm="Main"'
+        return response
+    
+    return wrapper
+    
 
 
 class ProductListView(ListView):
@@ -18,22 +40,33 @@ class ProductDetailView(DetailView):
         context['related_products'] = related_products
         return context
 
-
+@method_decorator(basic_auth_required, name='dispatch')
 class ProductCreateView(CreateView):
     model = Product
     fields = ['name', 'description', 'price', 'image']
     template_name = 'product/product_create.html'
-    success_url = reverse_lazy('product:product_list')
+    success_url = reverse_lazy('product:manage_list')
 
 
+@method_decorator(basic_auth_required, name='dispatch')
 class ProductUpdateView(UpdateView):
     model = Product
     fields = ['name', 'description', 'price', 'image']
     template_name = 'product/product_update.html'
-    success_url = reverse_lazy('product:product_list')
+    success_url = reverse_lazy('product:manage_list')
 
 
+@method_decorator(basic_auth_required, name='dispatch')
 class ProductDeleteView(DeleteView):
     model = Product
     template_name = 'product/product_delete.html'
-    success_url = reverse_lazy('product:product_list')
+    success_url = reverse_lazy('product:manage_list')
+
+
+@method_decorator(basic_auth_required, name='dispatch')
+class ProductManageListView(ListView):
+    model = Product
+    template_name = 'product/product_manage_list.html'
+    context_object_name = 'manage_list'
+
+
